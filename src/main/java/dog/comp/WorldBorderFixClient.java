4b45a3711d8515e315b712e319a466f605341d3b
compdoge <sznyrowskii@gmail.com>
2025-06-02 17:41:41 +0200

    initial

---
+++ src/main/java/dog/comp/WorldBorderFixClient.java
@@ -0,0 +1,11 @@
+package dog.comp;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WorldBorderFixClient implements ClientModInitializer {
+  @Override
+  public void onInitializeClient() {
+    
+  }
+
+}
---
+++ src/main/java/dog/comp/WorldBorderFixDataGenerator.java
@@ -0,0 +1,11 @@
+package dog.comp;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class WorldBorderFixDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
