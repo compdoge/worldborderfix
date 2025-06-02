fe1c9f775b8aee3245be88eccde7ef207c320649
compdoge <sznyrowskii@gmail.com>
2025-06-02 18:06:54 +0200

    finished the command

--- src/main/java/dog/comp/WorldBorderFix.java
+++ src/main/java/dog/comp/WorldBorderFix.java
@@ -1,17 +1,21 @@
 package dog.comp;
 
+import dog.comp.command.NetherBorder;
 import net.fabricmc.api.ModInitializer;
-
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class WorldBorderFix implements ModInitializer {
-	public static final String MOD_ID = "worldborderfix";
+    public static final String MOD_ID = "worldborderfix";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    @Override
+    public void onInitialize() {
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+            NetherBorder.register(dispatcher);
+        });
 
-	@Override
-	public void onInitialize() {
-		
-	}
+        LOGGER.info("WorldBorderFix mod initialized.");
+    }
 }
---
+++ src/main/java/dog/comp/command/NetherBorder.java
@@ -0,0 +1,33 @@
+package dog.comp.command;
+
+import com.mojang.brigadier.CommandDispatcher;
+import net.minecraft.command.argument.ColumnPosArgumentType;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.ColumnPos;
+import net.minecraft.world.border.WorldBorder;
+import net.minecraft.text.Text;
+
+public class NetherBorder {
+    public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
+        dispatcher.register(CommandManager.literal("netherborder")
+                .then(CommandManager.argument("pos", ColumnPosArgumentType.columnPos())
+                        .executes(context -> {
+                            ColumnPos pos = ColumnPosArgumentType.getColumnPos(context, "pos");
+
+                            double x = pos.x() * 8.0;
+                            double z = pos.z() * 8.0;
+
+                            ServerWorld overworld = context.getSource().getServer().getOverworld();
+                            WorldBorder border = overworld.getWorldBorder();
+
+                            border.setCenter(x, z);
+
+                            context.getSource().sendFeedback(
+                                    () -> Text.literal("World border center set to (" + x + ", " + z + ")"),
+                                    true);
+                            return 1;
+                        })));
+    }
+}
